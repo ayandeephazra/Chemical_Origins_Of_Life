b8c9899a28e15cd723ff42b27e628506d2a0ef5a
Ayandeep Hazra <ayandeephazra@hotmail.com>
2021-08-10 15:24:12 -0500

    example3

--- PysindyODEGen.py
+++ PysindyODEGen.py
@@ -16,7 +16,7 @@ def lorenz(z, t):
         z[0] * (28 - z[2]) - z[1],
         z[0] * z[1] - (8 / 3) * z[2]
     ]
-np.random.seed(100)
+#.random.seed(100)
 print("")
 # initial concentrations Glycine to Alanine
 # G A GG AA AG/GA GAG/GGA GGG GGGG AAG/AGA AAA
@@ -38,12 +38,12 @@ x = 3 * np.exp(-2 * t)
 y = 0.5 * np.exp(t)
   # First column is x, second is y
 
-y100_init = [0.1, 0, 0, 0, 0]
-y100_1 = [0.1, 0.068516, 0.068512554, 0.068512653, 0.06851261]
-y100_2 = [0, 0, 0, 0, 0]
-y100_3 = [0, 0.010187, 0.012749887, 0.012749946, 0.01274996]
-y100_4 = [0, 0, 0, 0, 0]
-y100_5 = [0, 0, 0, 0, 0]
+y100_init = [0.09, 0.01, 0, 0, 0]
+y100_1 = [0.075, 0.053246277, 0.053244499, 0.053244498, 0.053244501]
+y100_2 = [0.025, 0.019920837, 0.019919003, 0.019919049, 0.019919044]
+y100_3 = [0, 0.007370377, 0.007369057, 0.007369122, 0.00736912]
+y100_4 = [0, 6.38e-04, 6.38e-04, 6.38e-04, 6.38e-04]
+y100_5 = [0, 2.63e-03, 2.63e-03, 2.63e-03, 2.63e-03]
 
 X = np.stack((y100_1, y100_2, y100_3, y100_4, y100_5), axis=-1)
 differentiation_method = ps.FiniteDifference(order=2)
@@ -66,38 +66,39 @@ model.equations(precision=5)
 model.print()
 print("\n")
 
-for i in range(2):
-    y100_init = [0.1, 0, 0, 0, 0]
-    y100_1 = [0.1, 0.068516, 0.068512554, 0.068512653, 0.06851261]
-    y100_2 = [0, 0, 0, 0, 0]
-    y100_3 = [0, 0.010187, 0.012749887, 0.012749946, 0.01274996]
-    y100_4 = [0, 0, 0, 0, 0]
-    y100_5 = [0, 0, 0, 0, 0]
+for i in range(10):
+    y100_init = [0.09, 0.01, 0, 0, 0]
+    y100_1 = [0.075, 0.053246277, 0.053244499, 0.053244498, 0.053244501]
+    y100_2 = [0.025, 0.019920837, 0.019919003, 0.019919049, 0.019919044]
+    y100_3 = [0, 0.007370377, 0.007369057, 0.007369122, 0.00736912]
+    y100_4 = [0, 6.38e-04, 6.38e-04, 6.38e-04, 6.38e-04]
+    y100_5 = [0, 2.63e-03, 2.63e-03, 2.63e-03, 2.63e-03]
+
 
     X = np.stack((y100_1, y100_2, y100_3, y100_4, y100_5), axis=-1)
 
-    t1 = np.random.normal(loc=0, scale=0.5, size=5)
+    t1 = np.random.normal(loc=0, scale=0.25, size=5)
     #t1[t1<0] = 0
-    t2 = np.random.normal(loc=0, scale=0.5, size=5)
+    t2 = np.random.normal(loc=0, scale=0.25, size=5)
     #t2[t2 < 0] = 0
-    t3 = np.random.normal(loc=0, scale=0.5, size=5)
+    t3 = np.random.normal(loc=0, scale=0.25, size=5)
     #t3[t3 < 0] = 0
-    t4 = np.random.normal(loc=0, scale=0.5, size=5)
+    t4 = np.random.normal(loc=0, scale=0.25, size=5)
     #t4[t4 < 0] = 0
-    t5 = np.random.normal(loc=0, scale=0.5, size=5)
+    t5 = np.random.normal(loc=0, scale=0.25, size=5)
     #t5[t5 < 0] = 0
 
     noise = np.stack((t1, t2, t3, t4, t5), axis=-1)
-    print(X, "\n\n", noise, "\n\n", X*noise, "\n")
+    print(X, "\n\n", noise.round(5), "\n\n", (X*noise).round(5), "\n")
     X = X+ X*noise
     X[X < 0] = 0
-    print(X, "\n")
+    print(X.round(5), "\n")
     differentiation_method = ps.FiniteDifference(order=2)
 
     feature_library = ps.PolynomialLibrary(degree=3)
 
     #use 0.05 for good clean results
-    optimizer = ps.STLSQ(threshold=0.001)
+    optimizer = ps.STLSQ(threshold=0.0002)
 
     model = ps.SINDy(
         differentiation_method=differentiation_method,
