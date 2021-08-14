cc94363ffb6c951a7e65f96bac043445e192e3d3
Ayandeep Hazra <ayandeephazra@hotmail.com>
2021-08-14 12:45:07 -0500

    venv update

--- IVPSolver.py
+++ IVPSolver.py
@@ -3,21 +3,28 @@ from scipy.integrate import solve_ivp
 from ode_helpers import state_plotter
 import numpy as np
 
+np.set_printoptions(suppress=True)
+
 
 def f(t, y, c):
-    dydt = [0, 7 * y[3] - 19.5 * y[1] ** 2 - 9.5 * y[1] * y[2] + 4.5 * y[5] - 10 * y[1] * y[5] - 9.75 * y[1] * y[3] - 9.75 * y[1] * y[4],
-            9 * y[4] - 16 * y[2] ** 2 - 9.5 * y[1] * y[2] + 4.5 * y[5] - 10 * y[1] * y[2] - 4 * y[5] * y[2] - 1.75 * y[2] * y[4],
+    dydt = [1, 7 * y[3] - 19.5 * y[1] ** 2 - 9.5 * y[1] * y[2] + 4.5 * y[5] - 10 * y[1] * y[5] - 9.75 * y[1] * y[
+        3] - 9.75 * y[1] * y[4],
+            9 * y[4] - 16 * y[2] ** 2 - 9.5 * y[1] * y[2] + 4.5 * y[5] - 10 * y[1] * y[2] - 4 * y[5] * y[2] - 1.75 * y[
+                2] * y[4],
             9.75 * y[1] ** 2 - 3.5 * y[3] + 9.75 * y[1] * y[3] - 19.5 * y[3] ** 2 - 10 * y[2] * y[3],
             8 * y[2] ** 2 - 4.5 * y[4] - 9.75 * y[1] * y[4] - 1.75 * y[2] * y[4],
             9.5 * y[1] * y[2] - 4.5 * y[5] - 10 * y[1] * y[5] - 4 * y[5] * y[2]]
     return dydt
 
-tspan = np.linspace(0, 5, 5)
-yinit = [0, 0.1, 0, 0, 0, 0]
+
+tspan = np.linspace(0, 4, 5)
+yinit = [0, 0.05, 0.05, 0, 0, 0]
 c = [4, 3, -2, 0.5]
 
 sol = solve_ivp(lambda t, y: f(t, y, c),
-                [tspan[0], tspan[-1]], yinit, t_eval=tspan, rtol = 1e-5)
+                [tspan[0], tspan[-1]], yinit, t_eval=tspan, rtol=1e-5)
+import matplotlib.rcsetup as rcsetup
 
-print(sol.y)
+print(rcsetup.all_backends)
+print(sol.y.round(6))
 state_plotter(sol.t, sol.y, 1)
